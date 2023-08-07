c3781c07c917695d16238a4f12b952ff8bd7c196
JuliansRecent <juliansrecent@gmail.com>
2023-08-06 22:02:53 -0700

    Add links from the SplashPage component to the SignUp/SignIn components. Alter the fields for the SignUp component.

--- src/components/main/sign-up/SignUp.js
+++ src/components/main/sign-up/SignUp.js
@@ -2,59 +2,65 @@ import { Link } from 'react-router-dom'
 
 function SignUp(props) {
     return (
-		<div className='container-fluid d-flex justify-content-center align-items-center'>
-			<div className='w-100 d-flex flex-column justify-content-center align-items-center'>
-				<h1 className='trademark fs-4'>Track It</h1>
-				<div className='login-container d-flex flex-column justify-content-center'>
-					<div className='welcome-back-container flex-column d-flex justify-content-center align-items-center'>
-						<div className='w-100 title-text-container d-flex flex-column gap-2'>
-							<h2 className='fs-2'>Sign up!</h2>
-							<p className='get-started-text'>
-								Please enter your information below!
-							</p>
-						</div>
-						<div className='flex-column d-flex justify-content-center gap-2 w-100 mb-3'>
-							<span>Email</span>
-							<input
-								type='text'
-								placeholder='Enter your email'
-								className='form-control'
-							/>
-							<span>Password</span>
-							<input
-								type='password'
-								placeholder='Enter your password'
-								className='form-control'
-							/>
-							<span>Confirm Password</span>
-							<input
-								type='password'
-								placeholder='Enter your password again'
-								className='form-control'
-							/>
-							<span>Agent Pin</span>
-							<input
-								type='text'
-								placeholder="Enter your agent's pin"
-								className='form-control'
-							/>
-						</div>
-						<div className='w-100 d-flex flex-column justify-content-center gap-3'>
-							<Link to='/dashboard'>
-								<button className='btn login-btn w-100'>Sign up</button>
-							</Link>
-							<p className='no-acct-text text-center'>
-								Already have an account?{' '}
-								<Link to='/sign-in' className='sign-up-text'>
-									Login
+			<div className='container-fluid d-flex justify-content-center align-items-center'>
+				<div className='w-100 d-flex flex-column justify-content-center align-items-center'>
+					<h1 className='trademark fs-4'>Track It</h1>
+					<div className='login-container d-flex flex-column justify-content-center'>
+						<div className='welcome-back-container flex-column d-flex justify-content-center align-items-center'>
+							<div className='w-100 title-text-container d-flex flex-column gap-2'>
+								<h2 className='fs-2'>Sign up</h2>
+								<p className='get-started-text'>
+									Please enter your information below
+								</p>
+							</div>
+							<div className='flex-column d-flex justify-content-center gap-2 w-100 mb-3'>
+								<span>First Name</span>
+								<input
+									type='text'
+									placeholder='Enter your email'
+									className='form-control'
+								/>
+								<span>Last Name</span>
+								<input
+									type='text'
+									placeholder='Enter your email'
+									className='form-control'
+								/>
+								<span>Email</span>
+								<input
+									type='text'
+									placeholder='Enter your email'
+									className='form-control'
+								/>
+								<span>Password</span>
+								<input
+									type='password'
+									placeholder='Enter your password'
+									className='form-control'
+								/>
+								<span>Confirm Password</span>
+								<input
+									type='password'
+									placeholder='Enter your password again'
+									className='form-control'
+								/>
+							</div>
+							<div className='w-100 d-flex flex-column justify-content-center gap-3'>
+								<Link to='/dashboard'>
+									<button className='btn login-btn w-100'>Sign up</button>
 								</Link>
-							</p>
+								<p className='no-acct-text text-center'>
+									Already have an account?{' '}
+									<Link to='/sign-in' className='sign-up-text'>
+										Login
+									</Link>
+								</p>
+							</div>
 						</div>
 					</div>
 				</div>
 			</div>
-		</div>
-	);
+		);
 }
 
 export default SignUp;
--- src/components/main/splash-page/SplashPage.js
+++ src/components/main/splash-page/SplashPage.js
@@ -3,8 +3,20 @@ import { Link } from 'react-router-dom'
 
 function SplashPage(props) {
 	return (
-		<div>
-			
+		<div className='splash-container d-flex flex-column pt-5'>
+			<h1 className='trademark fs-4'>Track It</h1>
+			<Link to='/sign-in'>
+				<h2 className='sign_in_btn btn'>Sign in</h2>
+			</Link>
+			<div className='text-left'>
+				<span className='d-block title-text mb-3'>
+					The only analytics platform
+				</span>
+				<span className='d-block title-text mb-5'>you'll ever need</span>
+			</div>
+			<Link to='/sign-up'>
+				<button className='btn bg-dark text-white '>Get Started</button>
+			</Link>
 		</div>
 	);
 }
